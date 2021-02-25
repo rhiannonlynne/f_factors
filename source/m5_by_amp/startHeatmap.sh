255304ec0857ac652e4a68a68967bd7fb697bb02
Bo Xin <bxin@lsst.org>
2021-02-25 16:35:14 -0700

    enable running bokeh locally

--- source/m5_by_amp/startHeatmap.sh
+++ source/m5_by_amp/startHeatmap.sh
@@ -1,5 +1,6 @@
 #source /opt/lsst/software/stack/loadLSST.bash; setup lsst_distrib
-export PYTHONPATH=$HOME/notebooks/f_factors/source/m5_by_amp/:$HOME/notebooks/datacat-utilities/python:$HOME/notebooks/eTraveler-clientAPI/python:$HOME/notebooks/EO-plotsDisplays/python
-bokeh serve $HOME/notebooks/EO-plotsDisplays/python/serveRenderFP.py --allow-websocket-origin=lsst-lsp-stable.ncsa.illinois.edu --args -e SR_emulation_config.txt --hook m5_hook -t "User m5 u"
+export PYTHONPATH=$HOME/notebooks/f_factors/source/m5_by_amp/:$HOME/notebooks/datacat-utilities/python:$HOME/notebooks/eTraveler-clientAPI/python:$HOME/notebooks/EO-plotsDisplays/python:$PYTHONPATH
+bokeh serve $HOME/notebooks/EO-plotsDisplays/python/serveRenderFP.py --allow-websocket-origin=localhost:5006 --args -e SR_emulation_config.txt --hook m5_hook -t "User m5 u"
+#bokeh serve $HOME/notebooks/EO-plotsDisplays/python/serveRenderFP.py --allow-websocket-origin=lsst-lsp-stable.ncsa.illinois.edu --args -e SR_emulation_config.txt --hook m5_hook -t "User m5 u"
 #bokeh serve $HOME/notebooks/EO-plotsDisplays/python/serveRenderFP.py --allow-websocket-origin=tucson-teststand.lsst.codes --args -e SR_emulation_config.txt --hook m5_hook -t "User m5 u"
 #bokeh serve $HOME/notebooks/EO-plotsDisplays/python/serveRenderFP.py --allow-websocket-origin=tucson-teststand.lsst.codes --args -e SR_emulation_config.txt --hook t_user_hook -t User
